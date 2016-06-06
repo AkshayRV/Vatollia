ee317fe0dca36d024bf7c54c8d1cb013665af2b8
AkshayRV <akshay.raj921@gmail.com>
2016-06-06 22:00:40 +0530

    Update and rename README.md to example.c

---
+++ example.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+ long long int a;
+ scanf("%d",a);
+ if(a==0)
+ printf("number is zero");
+ else if(a>0)
+ printf("number is positive");
+ else if(a<0)
+ printf("number is negative");
+ }
